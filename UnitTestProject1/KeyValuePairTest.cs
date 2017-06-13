dfdd8194c1364bd54330b23cbc16378637e472ec
Vincent Bosman <333302@student.fontys.nl>
2017-06-13 13:22:54 +0200

    Revert "put all the project files in its own directory"
    This reverts commit aa9a3a41b598a468d39b847209caca5124e6307b.

---
+++ UnitTestProject1/KeyValuePairTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MessageQueue;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MessageQueueTests
+{
+    [TestClass]
+    class KeyValuePairTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            //KeyValuePair kvp = new KeyValuePair("bla", "alb");
+            //Assert.AreEqual("bla", kvp.Key);
+            //Assert.AreEqual("alb", kvp.Value);
+        }
+    }
+}
